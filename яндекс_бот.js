a6e520573085a7eb2633797efd178140e96a7c66
skainnet <40144867+skainnet@users.noreply.github.com>
2020-04-03 08:06:29 +0300

    Update яндекс_бот.js

--- яндекс_бот.js
+++ яндекс_бот.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Бот яндекс 02
+// @name         Yndex botinok
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -8,18 +8,24 @@
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
-let YInput=document.getElementsByName('text')[0];
-let btn=document.getElementsByClassName('button_theme_websearch')[0];
-let sW = document.text = ['Гобой', 'Кларнет', 'Саксофон', 'Флейта','Валторна','Фагот'];
-let i=getRandom(0, sW.length);
-let links=document.links;
+ let Input=document.getElementsByName('text')[0];
+ let btn=document.getElementsByClassName('button_theme_websearch')[0];
+
+ let sW = document.text = ['Гобой', 'Кларнет', 'Саксофон', 'Флейта','Валторна','Фагот'];
+ let searchWord = sW[getRandom(0, sW.length)];
+ let i=0;
+ let links=document.links;
 
 if (btn!=undefined){
   let timerId=setInterval(()=>{
 
-     YInput.value=sW[i];
+     Input.value+=searchWord[i];
         i++;
-        
+        if(i==searchWord.length){
+
+          clearInterval(timerId);
+          btn.click();
+        }
       },500);
   }
 else if(location.hostname == "yandex.ru"){
@@ -27,8 +33,12 @@ else if(location.hostname == "yandex.ru"){
     for (let i = 0; i<links.length; i++){
     if (links[i].href.indexOf('https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/')!=-1)
     {
-      
-  }
+        links[i].removeAttribute("target");
+        flag=false;
+        links[i].click();
+        break;
+    }
+   }
     if(flag){
        setTimeout(()=>{
         if (document.querySelector('span[aria-label="Текущая Страница 11"]').innerText<11){
@@ -38,18 +48,21 @@ else if(location.hostname == "yandex.ru"){
             location.href = "https://yandex.ru/";
         }
     },2000);
-  }
+   }
 }
 else
 {
    setInterval(()=>
    {
-       if (getRandom(0,100)<30) location.href="https://yandex.ru/";
+
        let index=getRandom(0,links.length)
+       console.log (links[index]);
+       if (links[index].hostname != 'https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai'){
+       location.href="https://yandex.ru";;
+     }
        links[index].click();},1000);
    }
 
-function getRandom(min,max){
+    function getRandom(min,max){
     return Math.floor(Math.random()*(max-min)+min);
 }
-
